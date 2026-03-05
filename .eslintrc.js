adf72ddfa2876b267577486cda4c5fbfbbed4d74
Dima Goman <d.homan@evadav.com>
2026-03-05 14:37:05 +0300

    Initial monorepo setup with NestJS server

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    root: true,
+    env: { node: true, browser: true },
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    rules: {}
+  };
